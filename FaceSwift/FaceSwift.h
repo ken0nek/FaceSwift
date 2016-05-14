525e3c1bffc95a549272e87b4c53d9c1df6cf34c
ken0nek <ken.for.develop@gmail.com>
2016-05-14 15:14:54 +0900

    Initial Commit

---
+++ FaceSwift/FaceSwift.h
@@ -0,0 +1,19 @@
+//
+//  FaceSwift.h
+//  FaceSwift
+//
+//  Created by Ken Tominaga on 5/14/16.
+//  Copyright © 2016 ken0nek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FaceSwift.
+FOUNDATION_EXPORT double FaceSwiftVersionNumber;
+
+//! Project version string for FaceSwift.
+FOUNDATION_EXPORT const unsigned char FaceSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceSwift/PublicHeader.h>
+
+
